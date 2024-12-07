33faffc59ac7ac36be98e3c338ad6e233a090ab6
aliu <andrew.yc.liu@gmail.com>
2024-12-07 10:03:27 -0800

    whatever 2.5x speedup

--- 7/p2.cpp
+++ 7/p2.cpp
@@ -1,8 +1,12 @@
+#include <chrono>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <sstream>
+
+#include <thread>
+#include <atomic> //screw it
 
 //dfs it out
 bool num_works(long long calibration_val, std::vector<long long> vals)
@@ -22,12 +26,21 @@ bool num_works(long long calibration_val, std::vector<long long> vals)
     return mul || add || concat;
 }
 
+void thread_worker(long long calibration_val, std::vector<long long> vals, std::atomic_llong &out, std::atomic_int &done)
+{
+    long long temp = num_works(calibration_val, vals) * calibration_val;
+    out.fetch_add(temp);
+    done.fetch_add(1);
+}
+
 int main()
 {
     std::ifstream in("input.txt");
     std::string line;
 
-    long long p2 = 0;
+    std::atomic_llong p2 = 0;
+    std::atomic_int counter = 0;
+    int line_counter = 0;
     while (getline(in, line)) {
         std::stringstream linestream(line);
         std::string calibration_val_str;
@@ -40,7 +53,13 @@ int main()
         while (linestream >> temp) {
             vals.push_back(temp);
         }
-        p2 += calibration_val * num_works(calibration_val, vals);
+        std::thread thread(thread_worker, calibration_val, vals, std::ref(p2), std::ref(counter));
+        thread.detach();
+        line_counter++;
     }
+
+    while (counter < line_counter)
+        std::this_thread::sleep_for(std::chrono::microseconds(1));
+
     std::cout << "p2: " << p2 << '\n';
 }
