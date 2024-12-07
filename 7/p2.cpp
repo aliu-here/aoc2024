b8b6b1dd0533587df4b2dc5436cbf82fcfdd56ea
aliu <andrew.yc.liu@gmail.com>
2024-12-07 10:15:31 -0800

    Revert "~5x faster from early returns"
    This reverts commit faff1780039043ba0b70bf3b77b955edc1c41052.

--- 7/p2.cpp
+++ 7/p2.cpp
@@ -1,9 +1,12 @@
+#include <chrono>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <sstream>
 
+#include <thread>
+#include <atomic> //screw it
 
 //dfs it out
 bool num_works(long long calibration_val, std::vector<long long> vals)
@@ -14,27 +17,29 @@ bool num_works(long long calibration_val, std::vector<long long> vals)
         return calibration_val == vals[0];
     bool mul, add, concat;
     std::vector<long long> newvec(vals.begin()+1, vals.end());
-    newvec[0] = vals[0]+vals[1];
-    if (num_works(calibration_val, newvec))
-        return true;
-
     newvec[0] = vals[0]*vals[1];
-    if (num_works(calibration_val, newvec))
-        return true;
-
+    mul = num_works(calibration_val, newvec);
+    newvec[0] = vals[0]+vals[1];
+    add = num_works(calibration_val, newvec);
     newvec[0] = std::stol(std::to_string(vals[0]) + std::to_string(vals[1]));
-    if (num_works(calibration_val, newvec))
-        return true;
+    concat = num_works(calibration_val, newvec);
     return mul || add || concat;
 }
 
+void thread_worker(long long calibration_val, std::vector<long long> vals, std::atomic_llong &out, std::atomic_int &done)
+{
+    long long temp = num_works(calibration_val, vals) * calibration_val;
+    out.fetch_add(temp);
+    done.fetch_add(1);
+}
 
 int main()
 {
     std::ifstream in("input.txt");
     std::string line;
 
-    long long p2 = 0;
+    std::atomic_llong p2 = 0;
+    std::atomic_int counter = 0;
     int line_counter = 0;
     while (getline(in, line)) {
         std::stringstream linestream(line);
@@ -48,8 +53,13 @@ int main()
         while (linestream >> temp) {
             vals.push_back(temp);
         }
-        p2 += num_works(calibration_val, vals) * calibration_val;
+        std::thread thread(thread_worker, calibration_val, vals, std::ref(p2), std::ref(counter));
+        thread.detach();
+        line_counter++;
     }
+
+    while (counter < line_counter)
+        std::this_thread::sleep_for(std::chrono::microseconds(1));
 
     std::cout << "p2: " << p2 << '\n';
 }
