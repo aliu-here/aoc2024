51415deec32fe6348a74f39bd42f606c1f9d73c6
aliu <andrew.yc.liu@gmail.com>
2024-12-07 10:51:10 -0800

    removing mt speeds up by 2x

--- 7/p2.cpp
+++ 7/p2.cpp
@@ -5,10 +5,6 @@
 #include <sstream>
 #include <cmath>
 
-#include <thread>
-#include <atomic>
-#include <chrono>
-
 
 long long deconcat(long long a, long long b)
 {
@@ -44,22 +40,13 @@ bool num_works(long long calibration_val, std::vector<long long> vals)
     return out;
 }
 
-void thread_worker(long long calibration_val, std::vector<long long> vals, std::atomic_llong &out, std::atomic_int &counter)
-{
-    long long temp = num_works(calibration_val, vals) * calibration_val;
-//    std::cout << temp << '\n';
-    out.fetch_add(temp);
-    counter.fetch_add(1);
-}
 
 int main()
 {
     std::ifstream in("input.txt");
     std::string line;
 
-    std::atomic_llong p2 = 0;
-    std::atomic_int counter = 0;
-    int line_count = 0;
+    long long p2 = 0;
     while (getline(in, line)) {
         std::stringstream linestream(line);
         std::string calibration_val_str;
@@ -72,13 +59,8 @@ int main()
         while (linestream >> temp) {
             vals.push_back(temp);
         }
-        std::thread thread(thread_worker, calibration_val, vals, std::ref(p2), std::ref(counter));
-        thread.detach();
-        line_count++;
+        p2 += num_works(calibration_val, vals) * calibration_val;
     }
-
-    while (counter < line_count)
-        std::this_thread::sleep_for(std::chrono::microseconds(1));
 
     std::cout << "p2: " << p2 << '\n';
 }
